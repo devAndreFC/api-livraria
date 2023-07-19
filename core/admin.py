57e12981ad99d41de40cdc5ff45505c21aa55a69
Andre Oliveira <dev.andrefc@gmail.com>
2023-07-19 09:03:48 -0300

    feat: Adicionado models de categorias, editoras, autores, livros, compras, itens da compra e usuário.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Publisher, Author, Book, Purchase, ItemsPurchase
 
-# Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Publisher)
+
+
+class ItemsInline(admin.TabularInline):
+    model = ItemsPurchase
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    inlines = (ItemsInline, )
--- core/models.py
+++ core/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    site = models.URLField()
+
+    class Meta:
+        verbose_name = 'Publisher'
+        verbose_name_plural = 'Publishers'
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)    
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    tittle = models.CharField(max_length=255, unique=True)
+    ISBN = models.CharField(max_length=50)
+    quantify = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name= 'books')
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, related_name='books')
+    authors = models.ManyToManyField(Author, related_name='Books')
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return f'{self.tittle} ({self.publisher})'
+
+
+class Purchase(models.Model):
+    class StatusPurchase(models.IntegerChoices):
+        CART = 1, 'Cart'
+        ORDER = 2, 'Order'
+        PAID  = 3, 'Paid'
+        DELIVERED = 4, 'Delivered'
+    
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='Purchases')
+    status = models.IntegerField(choices=StatusPurchase.choices, default=StatusPurchase.CART)
+
+    class Meta:
+        verbose_name = 'Purchase'
+        verbose_name_plural = 'Purchases'
+
+    def __str__(self):
+        return f'Purchse of {self.user}'
+
+
+class ItemsPurchase(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name='Items')
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='+')
+    quantify = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Purchase Items'
+        verbose_name_plural = 'Purchase Items'
+
+    def __str__(self):
+        return f'Purchse ({self.purchase})'
